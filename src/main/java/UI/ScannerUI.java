6896ca26d268201589192166bbee9b9fbbc82d1c
robert <robert.gontaru@outlook.com>
2024-02-22 14:35:49 +0000

    Made Camera and UI work
    I made like thingy

--- src/main/java/UI/ScannerUI.java
+++ src/main/java/UI/ScannerUI.java
@@ -1,4 +1,59 @@
 package UI;
 
-public class ScannerUI {
+import com.github.sarxos.webcam.*;
+import util.BarcodeEncoder;
+
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+
+public class ScannerUI extends JFrame{
+    private static final Webcam webcam = Webcam.getDefault();
+
+    private JPanel MainPanel;
+    private JButton takePictureButton;
+
+    public ScannerUI() {
+        takePictureButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                try {
+                    ImageIO.write(webcam.getImage(), "PNG", new File("first2.PNG"));
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                try {
+                    new BarcodeEncoder();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        webcam.setViewSize(WebcamResolution.VGA.getSize());
+        WebcamPanel panel = new WebcamPanel(webcam);
+        panel.setImageSizeDisplayed(true);
+
+        panel.setBackground(Color.YELLOW);
+        panel.setMirrored(false);
+        JFrame frame = new JFrame("ScannerUI");
+
+        frame.add(new ScannerUI().MainPanel).isBackgroundSet();
+        frame.add(panel, BorderLayout.SOUTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.repaint();
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
 }
--- src/main/java/util/BarcodeEncoder.java
+++ src/main/java/util/BarcodeEncoder.java
@@ -1,4 +1,31 @@
 package util;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 public class BarcodeEncoder {
+    static File path = new File("first2.PNG");
+    static Image image;
+
+    static {
+        try {
+            image = ImageIO.read(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public BarcodeEncoder() throws IOException {
+        int imageWidth = image.getWidth(null);
+        int imageHeight = image.getHeight(null);
+        System.out.println("Image Width: " + imageWidth + " Image Height: " + imageHeight);
+    }
+
+    public static void main(String[] args) throws IOException {
+        int imageWidth = image.getWidth(null);
+        int imageHeight = image.getHeight(null);
+        System.out.println("Image Width: " + imageWidth + " Image Height: " + imageHeight);
+    }
 }
