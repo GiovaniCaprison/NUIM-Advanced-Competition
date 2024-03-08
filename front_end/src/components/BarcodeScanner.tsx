efc7f090e12dcd1f05becf7cb3a7fff332ad2008
Louis Grennell <icantfindanadressname@gmail.com>
2024-03-08 11:22:00 +0000

    refactor

--- front_end/src/components/BarcodeScanner.tsx
+++ front_end/src/components/BarcodeScanner.tsx
@@ -1,44 +1,19 @@
-import { useRef, useState, useEffect, useCallback } from 'react';
+import { useRef, useEffect } from 'react';
 import Webcam from 'react-webcam';
-import { BrowserMultiFormatReader } from '@zxing/library';
-import { sendBarcodeToBackend } from '../services/apiService'; // Import the service
+import { useBarcodeDecoder } from '../hooks/useBarcodeDecoder'; // Import the custom hook
 
 const BarcodeScanner = () => {
     const webcamRef = useRef<Webcam>(null);
-    const [barcode, setBarcode] = useState<string>('');
-    const [lastSentBarcode, setLastSentBarcode] = useState<string>('');
-
-    const capture = useCallback(() => {
-        if (webcamRef.current) {
-            const imageSrc = webcamRef.current.getScreenshot();
-            if (imageSrc) {
-                const codeReader = new BrowserMultiFormatReader();
-                codeReader.decodeFromImage(undefined, imageSrc).then((result) => {
-                    const decodedBarcode = result.getText();
-                    setBarcode(decodedBarcode);
-                    if (decodedBarcode !== lastSentBarcode) {
-                        sendBarcodeToBackend({barcode: decodedBarcode})
-                            .then((response) => {
-                                console.log('Barcode sent successfully', response);
-                                setLastSentBarcode(decodedBarcode); // Update last sent barcode
-                            })
-                            .catch((error) => {
-                                console.error('Failed to send barcode', error);
-                            });
-                    }
-                }).catch(() => {
-                    // Handle errors or not found cases
-                });
-            }
-        }
-    }, [lastSentBarcode]);
+    const { decodeBarcode, barcode } = useBarcodeDecoder();
 
     useEffect(() => {
         const interval = setInterval(() => {
-            capture();
+            const imageSrc = webcamRef.current?.getScreenshot();
+            decodeBarcode(imageSrc);
         }, 1000); // Capture an image every second
+
         return () => clearInterval(interval);
-    }, [capture]);
+    }, [decodeBarcode]);
 
     return (
         <>
@@ -56,6 +31,8 @@ const BarcodeScanner = () => {
 };
 
 export default BarcodeScanner;
+
+
 
 
 
